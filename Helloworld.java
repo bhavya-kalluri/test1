dca786b8b4db36e1e9832415c844c96698f278c5
Bhavya reddy Kalluri <bhavyareddykalluri@Bhavyas-MacBook-Pro.local>
2020-08-13 12:24:11 -0400

    i value

--- Helloworld.java
+++ Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 		// TODO Auto-generated method stub
      for(int i=10;i>0;i--)
      {
-    	 System.out.println("Hello World"+i);
+    	 System.out.println(+i);
      }
 	}
 
